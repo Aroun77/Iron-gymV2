8d47f4125e3ef01be0ba4e24175ce9384fcea297
Aroun77 <gnanavelan.aroun@gmail.com>
2025-12-05 03:59:17 +0100

    fix: disable service worker to force cache refresh

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,3 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import 'virtual:uno.css' // Import UnoCSS styles
-
-// Register Service Worker for offline support
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then((registration) => {
-        console.log('SW registered:', registration);
-      })
-      .catch((error) => {
-        console.log('SW registration failed:', error);
-      });
-  });
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  </React.StrictMode >,
 )
